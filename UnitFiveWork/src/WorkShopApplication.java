f765f57b56852cff5d177f08d3eab773eb153651
alu20924612v <alu20924612v@ti2-10>
2018-03-21 19:55:54 +0100

    Conflict totally solved

--- UnitFiveWork/src/WorkShopApplication.java
+++ UnitFiveWork/src/WorkShopApplication.java
@@ -1,21 +1,11 @@
+import java.util.Date;
+
 public class WorkShopApplication {
 
-<<<<<<< HEAD
+
 	private Product1 product1 = new Product1(new Product());
 	private String cliFirstName;
 	
-	//Invoice information
-	private int codInvoice;
-	private Date dateInvoice;
-	private double sum;
-	private int discount;
-	
-	//Invoice line information
-	private int numInvoiceLine;
-	private int numArticles;
-	private int codArt;
-	private int codQuantity;
-=======
 	private Invoice Invoice1 = new Invoice(new Product(), new Client());
->>>>>>> refs/heads/RefactorClientInvoice
+
 }
