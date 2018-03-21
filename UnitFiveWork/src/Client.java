ea4962be1eadef1ff6ed7395b727f1f9ad4f96d5
alu20924612v <alu20924612v@ti2-10>
2018-03-21 19:47:16 +0100

    Refactor of client and invoice

---
+++ UnitFiveWork/src/Client.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Client {
+	public String cliFirstName;
+	public int codInvoice;
+	public Date dateInvoice;
+	public double sum;
+
+	public Client() {
+	}
+}
--- UnitFiveWork/src/WorkShopApplication.java
+++ UnitFiveWork/src/WorkShopApplication.java
@@ -1,23 +1,4 @@
-import java.util.Date;
-
 public class WorkShopApplication {
 
-	private Product data = new Product();
-	//Client information
-	private int codCli;
-	private String cliDNI;
-	private String cliName;
-	private String cliFirstName;
-	
-	//Invoice information
-	private int codInvoice;
-	private Date dateInvoice;
-	private double sum;
-	private int discount;
-	
-	//Invoice line information
-	private int numInvoiceLine;
-	private int numArticles;
-	private int codArt;
-	private int codQuantity;
+	private Invoice Invoice1 = new Invoice(new Product(), new Client());
 }
